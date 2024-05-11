632fcca9552d70402aea22dc87228885f2437925
miletajovanovic <mileta.jovanovic01@gmail.com>
2024-05-11 19:06:16 +0200

    csfr defense

--- src/main/java/com/urosdragojevic/realbookstore/controller/PersonsController.java
+++ src/main/java/com/urosdragojevic/realbookstore/controller/PersonsController.java
@@ -1,6 +1,7 @@
 package com.urosdragojevic.realbookstore.controller;
 
 import com.urosdragojevic.realbookstore.audit.AuditLogger;
+
 import com.urosdragojevic.realbookstore.domain.Person;
 import com.urosdragojevic.realbookstore.domain.User;
 import com.urosdragojevic.realbookstore.repository.PersonRepository;
@@ -12,6 +13,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpSession;
+import java.nio.file.AccessDeniedException;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -31,7 +34,8 @@ public class PersonsController {
     }
 
     @GetMapping("/persons/{id}")
-    public String person(@PathVariable int id, Model model) {
+    public String person(@PathVariable int id, Model model, HttpSession session) {
+        model.addAttribute("CSRF_TOKEN", session.getAttribute("CSRF_TOKEN"));
         model.addAttribute("person", personRepository.get("" + id));
         return "person";
     }
@@ -52,8 +56,13 @@ public class PersonsController {
     }
 
     @PostMapping("/update-person")
-    public String updatePerson(Person person) {
-        personRepository.update(person);
+    public String updatePerson(Person person, HttpSession session, @RequestParam("csrfToken") String csrfToken) throws AccessDeniedException {
+    	String csrf = session.getAttribute("CSRF_TOKEN").toString();
+        if (!csrf.equals(csrfToken)) {
+            throw new AccessDeniedException("Forbidden");
+        }
+
+    	personRepository.update(person);
         return "redirect:/persons/" + person.getId();
     }
 
